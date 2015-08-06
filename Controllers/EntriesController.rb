4d51cd2b7a42c2d2fca35042fbcb83f9a348bc62
KateMShirley <kate.m.shirley@gmail.com>
2015-08-06 13:44:08 -0500

    add to alemanac from search results

--- Controllers/EntriesController.rb
+++ Controllers/EntriesController.rb
@@ -6,17 +6,46 @@ class EntriesController < ApplicationController
     erb :new_entry
   end
 
+  get '/new_api_entry' do
+    erb :new_entry_from_api
+  end
+
   post '/new_api_entry' do
 
     puts '---------'
     puts params
+    puts user = session[:user]
     puts '---------'
 
-    @beer_name = params['beer_name']
-    @beer_brewery = ''
-    @beer_abv = params['abv']
+    puts
+    @beer = EntriesModel.new
 
-    erb :new_entry_from_api
+    @beer.beer_name = params[:beer_name]
+    # @beer.brewery = params[:brewery_name]
+    @beer.abv = params[:abv].to_i
+    # @beer.brew_location = params[:brew_location]
+    @beer.consume_location = params[:consume_location]
+    @beer.vote = params[:vote].to_i
+    @beer.notes = params[:notes]
+    if params[:beer_id]
+      @beer.is_custom = false
+      @beer.beer_id = params[:beer_id].to_i
+    else
+      @beer.is_custom = true
+    end
+    @beer.user_id = user.id.to_i
+    # @beer.user_id = UsersModel.find_by(:id => id.to_i)
+
+    puts '---------'
+    puts @beer
+    puts '---------'
+    @beer.save
+
+    # @beer_name = params['beer_name']
+    # @beer_brewery = ''
+    # @beer_abv = params['abv']
+
+    erb :new_entry_success
 
   end
 
@@ -32,9 +61,9 @@ class EntriesController < ApplicationController
     @beer = EntriesModel.new
 
     @beer.beer_name = params[:beer_name]
-    @beer.brewery = params[:brewery_name]
+    # @beer.brewery = params[:brewery_name]
     @beer.abv = params[:abv].to_i
-    @beer.brew_location = params[:brew_location]
+    # @beer.brew_location = params[:brew_location]
     @beer.consume_location = params[:consume_location]
     @beer.vote = params[:vote].to_i
     @beer.notes = params[:notes]
